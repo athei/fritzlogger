fe9028d8f5ef08a7e3da9574c5cac71d970e447a
Alexander Theißen <alex@theissen.io>
2019-07-14 19:23:43 +0200

    Output both temperature and energy in csv module
    Now two files are created: Once for energy and one for
    temperature. Devices that have neither of those properties
    are ignored.

--- src/backend/csv.rs
+++ src/backend/csv.rs
@@ -12,24 +12,34 @@ use std::time::Duration;
 
 #[derive(Deserialize, Serialize)]
 pub struct Settings {
-    outfile: String,
+    out_dir: String,
 }
 
 impl<'de> settings::Settings<'de, Csv> for Settings {
     fn defaults() -> Vec<(String, Value)> {
-        vec![("outfile".into(), "./fritzaha.csv".into())]
+        vec![("out_dir".into(), ".".into())]
     }
 }
 
 pub struct Csv {
-    writer: Writer<File>,
+    writer_temperature: Writer<File>,
+    writer_energy: Writer<File>,
 }
 
 #[derive(Serialize)]
-struct Record<'a> {
+struct RecordTemperature<'a> {
     timestamp: u64,
     id: &'a str,
     temperature: i16,
+    offset: i16,
+}
+
+#[derive(Serialize)]
+struct RecordEnergy<'a> {
+    timestamp: u64,
+    id: &'a str,
+    voltage: u32,
+    power: u32,
 }
 
 impl<'de> Backend<'de> for Csv {
@@ -41,52 +51,104 @@ impl<'de> Backend<'de> for Csv {
 
     fn new(settings: Self::Settings) -> Result<Self> {
         let ret = Self {
-            writer: create_writer(&settings.outfile).chain_err(|| "Cannot open csv outfile")?,
+            writer_temperature: Self::create_writer(&format!(
+                "{}/temperature.csv",
+                &settings.out_dir
+            ))
+            .chain_err(|| "Cannot open temperature outfile")?,
+            writer_energy: Self::create_writer(&format!("{}/energy.csv", &settings.out_dir))
+                .chain_err(|| "Cannot open energy outfile")?,
         };
         Ok(ret)
     }
 
     fn log(&mut self, when: Duration, data: &[Device]) -> Result<()> {
-        let timestamp = when.as_secs();
-        let records = data.iter().map(|d| Record {
-            timestamp,
-            id: &d.common.unique_id,
-            temperature: d.temperature.as_ref().unwrap().temperature,
-        });
-
-        for record in records {
-            self.writer
-                .serialize(record)
-                .chain_err(|| "Error serializing csv record")?;
-        }
-        self.writer
-            .flush()
-            .chain_err(|| "Cannot flush out csv recors")?;
-
+        self.write_temperature(&when, data)?;
+        self.write_energy(&when, data)?;
         Ok(())
     }
 }
 
-fn create_writer(path: &str) -> std::result::Result<Writer<File>, std::io::Error> {
-    let mut fbuilder = OpenOptions::new();
-    let file_prexists;
+impl Csv {
+    fn write_temperature(&mut self, when: &Duration, data: &[Device]) -> Result<()> {
+        let timestamp = when.as_secs();
+        let records = data.iter().filter_map(|d| {
+            let temperature = match &d.temperature {
+                Some(ref temperature) => temperature,
+                _ => return None,
+            };
 
-    fbuilder.read(false).append(true).truncate(false);
-    let file = match fbuilder.create(false).open(path) {
-        Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => {
-            file_prexists = false;
-            fbuilder.create(true).open(path)
+            let record = RecordTemperature {
+                timestamp,
+                id: &d.common.unique_id,
+                temperature: temperature.temperature,
+                offset: temperature.offset,
+            };
+            Some(record)
+        });
+
+        for record in records {
+            self.writer_temperature
+                .serialize(record)
+                .chain_err(|| "Error serializing csv record")?;
         }
-        x @ Ok(_) => {
-            file_prexists = true;
-            x
+        self.writer_temperature
+            .flush()
+            .chain_err(|| "Cannot flush out csv records")?;
+
+        Ok(())
+    }
+
+    fn write_energy(&mut self, when: &Duration, data: &[Device]) -> Result<()> {
+        let timestamp = when.as_secs();
+        let records = data.iter().filter_map(|d| {
+            let energy = match &d.powermeter {
+                Some(energy) => energy,
+                _ => return None,
+            };
+
+            let record = RecordEnergy {
+                timestamp,
+                id: &d.common.unique_id,
+                voltage: energy.voltage,
+                power: energy.power,
+            };
+            Some(record)
+        });
+
+        for record in records {
+            self.writer_energy
+                .serialize(record)
+                .chain_err(|| "Error serializing csv record")?;
         }
-        x => {
-            file_prexists = false;
-            x
-        }
-    }?;
-    Ok(WriterBuilder::new()
-        .has_headers(!file_prexists)
-        .from_writer(file))
+        self.writer_energy
+            .flush()
+            .chain_err(|| "Cannot flush out csv records")?;
+
+        Ok(())
+    }
+
+    fn create_writer(path: &str) -> std::result::Result<Writer<File>, std::io::Error> {
+        let mut fbuilder = OpenOptions::new();
+        let file_prexists;
+
+        fbuilder.read(false).append(true).truncate(false);
+        let file = match fbuilder.create(false).open(path) {
+            Err(ref err) if err.kind() == std::io::ErrorKind::NotFound => {
+                file_prexists = false;
+                fbuilder.create(true).open(path)
+            }
+            x @ Ok(_) => {
+                file_prexists = true;
+                x
+            }
+            x => {
+                file_prexists = false;
+                x
+            }
+        }?;
+        Ok(WriterBuilder::new()
+            .has_headers(!file_prexists)
+            .from_writer(file))
+    }
 }
